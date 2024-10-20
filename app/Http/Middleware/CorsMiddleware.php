6a1e06ee1264cb47e0bd184b7e3f6580e7982483
Ascoor <94120646+Ascoor@users.noreply.github.com>
2024-10-20 09:16:35 +0300

    Update CorsMiddleware.php

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -20,8 +20,8 @@ class CorsMiddleware
         $allowedOrigins = [
             'http://localhost:3000',
             'http://127.0.0.1:3000',
-            'http://app.hadathah.org',
-            'https://app.hadathah.org'
+            'http://store.ve=shop.co',
+            'https://store.ve=shop.co'
         ];
         
         $origin = $request->headers->get('origin');
