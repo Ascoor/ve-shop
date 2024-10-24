a9113f5ea0091aa5cc87707862fcc5e178233bd7
askar <mr.askar1984@gmail.com>
2024-10-24 09:51:24 +0300

    zx

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->foreignId('role_id')->constrained()->onDelete('cascade')->default(3);
+            
+            $table->foreignId('role_id')
+            ->constrained()
+            ->onDelete('cascade')
+            ->default(3);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
