da47d72015aa7fc75ef056a66141668dd1f5e766
Ascoor <Ascoor@users.noreply.github.com>
2025-07-09 09:19:26 +0000

    deploy: 13f2b10079a62a8e7c69ac0c664df8ced1f288a9

--- assets/ProductGrid-D4Evu2gw.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BUr1vC0x.js";import{P as a}from"./ProductCard-C7rCfdrM.js";import{u as d}from"./catalogStore-Clnm04sg.js";const g=({categoryId:s="all"})=>{const t=d(r=>r.getProductsByCategory(s));return o.jsx("div",{className:"grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6",children:t.map(r=>o.jsx(a,{...r},r.id))})};export{g as P};
